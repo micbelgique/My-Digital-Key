3acb4dd0743c504900e5aaa4558d310be26ff568
moffa13 <moffajeremy@outlook.com>
2017-07-29 16:59:42 +0200

    Test

--- raspi/src/main.cpp
+++ raspi/src/main.cpp
@@ -42,23 +42,6 @@ main(int argc, const char *argv[])
 
 	printf("lol");
 
-	const nfc_modulation nmMifare = {
-		.nmt = NMT_ISO14443A,
-		.nbr = NBR_106,
-	};
-	if (nfc_initiator_select_passive_target(pnd, nmMifare, NULL, 0, &nt) > 0) {
-		printf("The following (NFC) ISO14443A tag was found:\n");
-		printf("    ATQA (SENS_RES): ");
-		print_hex(nt.nti.nai.abtAtqa, 2);
-		printf("       UID (NFCID%c): ", (nt.nti.nai.abtUid[0] == 0x08 ? '3' : '1'));
-		print_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen);
-		printf("      SAK (SEL_RES): ");
-		print_hex(&nt.nti.nai.btSak, 1);
-		if (nt.nti.nai.szAtsLen) {
-			printf("          ATS (ATR): ");
-			print_hex(nt.nti.nai.abtAts, nt.nti.nai.szAtsLen);
-		}
-	}
 	nfc_close(pnd);
 	nfc_exit(context);
 	exit(EXIT_SUCCESS);
